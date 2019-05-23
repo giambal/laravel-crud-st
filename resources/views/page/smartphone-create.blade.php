bdef94041f5500bf3eba6b8f526db47f64a3b96f
giambal <gimmy_1996@hotmail.it>
2019-05-23 17:33:04 +0200

    style

--- resources/views/page/smartphone-create.blade.php
+++ resources/views/page/smartphone-create.blade.php
@@ -2,30 +2,45 @@
 
 @section('content')
 
+<div class="title">
+  <div class="">
 
-    <form action="{{ route('st.store')}}" method="post">
-      @csrf
-      <div class="formDiv">
+  </div>
+  <div class="">
+    <h1>Add New Smartphone</h1>
 
-        <label for="brand">Brand</label><br>
-        <input type="text" name="brand" value="">
-      </div>
-      <div class="formDiv">
+  </div>
+  <div class="link">
 
-        <label for="model">Model</label><br>
-        <input type="text" name="model" value="">
-      </div>
-      <div class="formDiv">
+    <a href="{{route('st.index')}}">torna indietro</a>
+  </div>
+</div>
+<div class="container">
 
-        <label for="display">Display</label><br>
-        <input type="text" name="display" value="">
-      </div>
-      <div class="formDiv">
+  <form action="{{ route('st.store')}}" method="post">
+    @csrf
+    <div class="formDiv">
 
-        <label for="mem_ext">Max speed</label><br>
-        <input type="text" name="mem_ext" value="">
-      </div>
-      <button type="submit" name="button">create</button>
+      <label for="brand">Brand</label><br>
+      <input type="text" name="brand" value="">
+    </div>
+    <div class="formDiv">
+
+      <label for="model">Model</label><br>
+      <input type="text" name="model" value="">
+    </div>
+    <div class="formDiv">
+
+      <label for="display">Display</label><br>
+      <input type="text" name="display" value="">
+    </div>
+    <div class="formDiv">
+
+      <label for="mem_ext">Mem ext</label><br>
+      <input type="text" name="mem_ext" value="">
+    </div>
+    <button type="submit" name="">Save the smartphone</button>
   </form>
+</div>
 
 @stop
